c8192ce1892f19d84ee33c41808c62c3544051a4
Alade David <alade.david@email.com>
2026-03-31 18:24:53 +0100

    update colors

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -14,8 +14,8 @@ const TopDoctors = () => {
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
             {doctors.slice(0,10).map((item,index)=>(
-                <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
-                    <img className='bg-blue-50' src={item.image} alt="" />
+                <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-teal-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                    <img className='bg-teal-50' src={item.image} alt="" />
                     <div className='p-4'>
                         <div className='flex items-center gap-2 text-sm text-center text-green-500'>
                             {/* <p className={`w-2 h-2 rounded-full ${item.available ? 'bg-green-500' : 'bg-red-500'}`}></p><p>Available</p> */}
@@ -29,7 +29,7 @@ const TopDoctors = () => {
                 </div>
             ))} 
         </div>
-        <button onClick={()=>{navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
+        <button onClick={()=>{navigate('/doctors'); scrollTo(0,0)}} className='bg-teal-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
     </div>
   )
 }
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -42,14 +42,14 @@ const Doctors = () => {
         <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
           {
             filterDoc.map((item,index)=>(
-                <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
-                    <img className='bg-blue-50' src={item.image} alt="" />
+                <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-teal-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                    <img className='bg-teal-50' src={item.image} alt="" />
                     <div className='p-4'>
                         <div className='flex items-center gap-2 text-sm text-center text-green-500'>
                             {/* <p className={`w-2 h-2 rounded-full ${item.available ? 'bg-green-500' : 'bg-red-500'}`}></p><p>Available</p> */}
                             <p className={`w-2 h-2 rounded-full animate-pulse ${item.available ? 'bg-green-500 shadow-green-400' : 'bg-red-500 shadow-red-400'}`}></p>
-  <p className={`px-2 py-0.5 rounded-full text-xs font-medium ${item.available ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-600'}`}>
-    {item.available ? 'Available' : 'Unavailable'}</p>
+                            <p className={`px-2 py-0.5 rounded-full text-xs font-medium ${item.available ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-600'}`}>
+                              {item.available ? 'Available' : 'Unavailable'}</p>
                         </div>        
                         <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
                         <p className='text-gray-600 text-sm'>{item.speciality}</p>
