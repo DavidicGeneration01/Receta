8d9995f18f6ebe7e2cda0d1ab6497ec24b044690
Alade David <alade.david@email.com>
2026-06-26 19:43:26 +0100

    Addition Frontend Update

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -25,8 +25,8 @@ const RelatedDoctors = ({speciality,docId}) => {
                 <div onClick={()=>{navigate(`/appointment/${item._id}`); scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                     <img className='bg-blue-50' src={item.image} alt="" />
                     <div className='p-4'>
-                        <div className='flex items-center gap-2 text-sm text-center text-green-500'>
-                            <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                        <div className='flex items-center gap-2 text-sm text-center text-navy-500'>
+                            <p className='w-2 h-2 bg-navy-500 rounded-full'></p><p>Available</p>
                         </div>        
                         <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
                         <p className='text-gray-600 text-sm'>{item.speciality}</p>
@@ -34,7 +34,7 @@ const RelatedDoctors = ({speciality,docId}) => {
                 </div>
             ))} 
         </div>
-        <button onClick={()=>{navigate('/doctors'); scrollTo(0,0)}} className='bg-teal-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
+        <button onClick={()=>{navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
     </div>
   )
 }
